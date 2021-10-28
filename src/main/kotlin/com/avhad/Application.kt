0451ec9919091015dbbc256f608dbed1640ef8f7
Sagar <avhad.avhad@gmail.com>
2021-10-28 15:45:05 +0530

    Configured session management.

--- src/main/kotlin/com/avhad/Application.kt
+++ src/main/kotlin/com/avhad/Application.kt
@@ -3,12 +3,30 @@ package com.avhad
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.avhad.plugins.*
+import com.avhad.session.DrawingSession
+import io.ktor.application.*
+import io.ktor.sessions.*
+import io.ktor.util.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSessionManagement()
         configureRouting()
         configureSerialization()
         configureSockets()
         configureMonitoring()
     }.start(wait = true)
+}
+
+private fun Application.configureSessionManagement() {
+    install(Sessions) {
+        cookie<DrawingSession>("SESSION")
+    }
+    intercept(ApplicationCallPipeline.Features) {
+        if (call.sessions.get<DrawingSession>() == null) {
+
+            val clientId = call.parameters["client_id"] ?: ""
+            call.sessions.set(DrawingSession(clientId, generateNonce()))
+        }
+    }
 }
